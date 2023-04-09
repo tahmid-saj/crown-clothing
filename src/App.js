473e72b30d637744ae48fcd95dc1c324aa16b53f
Tahmid Sajin <tahmid.saj@gmail.com>
2023-04-09 01:41:59 -0400

    Updating directory page and category item component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,37 @@
-import './App.css';
+import Directory from './components/directory/directory.component';
 
 const App = () => {
+  const categories = [
+    {
+      id: 1,
+      title: "hats",
+      imageUrl: "https://i.ibb.co/cvpntL1/hats.png"
+    },
+    {
+      id: 2,
+      title: "jackets",
+      imageUrl: "https://i.ibb.co/px2tCc3/jackets.png"
+    },
+    {
+      id: 3,
+      title: "sneakers",
+      imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png"
+    },
+    {
+      id: 4,
+      title: "womens",
+      imageUrl: "https://media.istockphoto.com/id/916092484/photo/women-clothes-hanging-on-hangers-clothing-rails-fashion-design.jpg?s=612x612&w=0&k=20&c=fUpcbOITkQqitglZfgJkWO3py-jsbuhc8eZfb4sdrfE="
+    },
+    {
+      id: 5,
+      title: "mens",
+      imageUrl: "https://media.istockphoto.com/id/887360960/photo/mens-suits-on-hangers-in-different-colors.jpg?s=612x612&w=0&k=20&c=RR19yJjRuR-CBWj9u1sQkFgtdYJ07KEkM678R0mtuZY="
+    }
+  ];
+  
+
   return (
-    <div>
-      Hello World
-    </div>
+    <Directory categories={categories}/>
   );
 }
 
