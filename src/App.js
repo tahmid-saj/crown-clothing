d94da0b1cd03f804e6850294564b061511d46215
Tahmid Sajin <tahmid.saj@gmail.com>
2023-04-09 05:02:41 -0400

    Adding routing

--- src/App.js
+++ src/App.js
@@ -1,37 +1,20 @@
-import Directory from './components/directory/directory.component';
+import { Routes, Route, Outlet } from 'react-router-dom';
+import Home from './routes/home/home.component';
+import Navigation from './routes/navigation/navigation.component';
+
+const Shop = () => {
+  return <h1>I am the shop page</h1>;
+}
 
 const App = () => {
-  const categories = [
-    {
-      id: 1,
-      title: "hats",
-      imageUrl: "https://i.ibb.co/cvpntL1/hats.png"
-    },
-    {
-      id: 2,
-      title: "jackets",
-      imageUrl: "https://i.ibb.co/px2tCc3/jackets.png"
-    },
-    {
-      id: 3,
-      title: "sneakers",
-      imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png"
-    },
-    {
-      id: 4,
-      title: "womens",
-      imageUrl: "https://media.istockphoto.com/id/916092484/photo/women-clothes-hanging-on-hangers-clothing-rails-fashion-design.jpg?s=612x612&w=0&k=20&c=fUpcbOITkQqitglZfgJkWO3py-jsbuhc8eZfb4sdrfE="
-    },
-    {
-      id: 5,
-      title: "mens",
-      imageUrl: "https://media.istockphoto.com/id/887360960/photo/mens-suits-on-hangers-in-different-colors.jpg?s=612x612&w=0&k=20&c=RR19yJjRuR-CBWj9u1sQkFgtdYJ07KEkM678R0mtuZY="
-    }
-  ];
-  
-
   return (
-    <Directory categories={categories}/>
+    <Routes>
+      <Route path='/' element={<Navigation/>}>
+        <Route index={true} element={<Home/>}/>
+        <Route path='shop' element={<Shop/>}/>
+
+      </Route>
+    </Routes>
   );
 }
 
