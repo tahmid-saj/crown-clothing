bcdfccc27dbd4328ee08b93fa44cbd16f8385bdb
Tahmid Sajin <tahmid.saj@gmail.com>
2023-04-25 09:52:44 -0400

    Updating signin and sign up form

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,6 +1,10 @@
 import { useEffect } from 'react';
 import { getRedirectResult } from 'firebase/auth';
-import { auth, signInWithGooglePopup, createUserDocumentFromAuth, signInWithGoogleRedirect } from '../../utils/firebase/firebase.utils';
+import { auth, 
+    signInWithGooglePopup, 
+    createUserDocumentFromAuth, 
+    signInWithGoogleRedirect } from '../../utils/firebase/firebase.utils';
+import SignUpForm from '../../components/sign-up-form/sign-up-form.component';
 
 const SignIn = () => {
     // useEffect(async () => {
@@ -24,6 +28,8 @@ const SignIn = () => {
             <button onClick={logGoogleUser}>
                 Sign in with Google Popup
             </button>
+
+            <SignUpForm/>
         </div>
     );
 }
