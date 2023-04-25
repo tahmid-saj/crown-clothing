53d6113e784b21885a4929629f849dcf0dabf35f
Tahmid Sajin <tahmid.saj@gmail.com>
2023-04-25 08:56:14 -0400

    Adding nav, routing and firebase API call for signin

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Routes, Route, Outlet } from 'react-router-dom';
 import Home from './routes/home/home.component';
 import Navigation from './routes/navigation/navigation.component';
+import SignIn from './routes/sign-in/sign-in.component';
 
 const Shop = () => {
   return <h1>I am the shop page</h1>;
@@ -11,7 +12,7 @@ const App = () => {
     <Routes>
       <Route path='/' element={<Navigation/>}>
         <Route index={true} element={<Home/>}/>
-        <Route path='shop' element={<Shop/>}/>
+        <Route path='sign-in' element={<SignIn/>}/>
 
       </Route>
     </Routes>
---
+++ src/routes/sign-in/sign-in.component.jsx
@@ -0,0 +1,31 @@
+import { useEffect } from 'react';
+import { getRedirectResult } from 'firebase/auth';
+import { auth, signInWithGooglePopup, createUserDocumentFromAuth, signInWithGoogleRedirect } from '../../utils/firebase/firebase.utils';
+
+const SignIn = () => {
+    // useEffect(async () => {
+    //     const response = await getRedirectResult(auth);
+
+    //     if (response) {
+    //         const userDocRef = await createUserDocumentFromAuth(response.user);
+    //     }
+    // }, []);
+
+    const logGoogleUser = async() => {
+        const { user } = await signInWithGooglePopup();
+        const userDocRef = await createUserDocumentFromAuth(user)
+
+    };
+
+    return (
+        <div>
+            <h1>Sign In Page</h1>
+
+            <button onClick={logGoogleUser}>
+                Sign in with Google Popup
+            </button>
+        </div>
+    );
+}
+
+export default SignIn;
