b1cb81f7a9dcfc8ed59d6bfa453ada30eb68cfa8
Tahmid Sajin <tahmid.saj@gmail.com>
2023-04-25 23:31:54 -0400

    Adding sign-in / sign-up cleanup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { Routes, Route, Outlet } from 'react-router-dom';
 import Home from './routes/home/home.component';
 import Navigation from './routes/navigation/navigation.component';
-import SignIn from './routes/sign-in/sign-in.component';
+import Authentication from './routes/authentication/authentication.component';
 
 const Shop = () => {
   return <h1>I am the shop page</h1>;
@@ -12,7 +12,8 @@ const App = () => {
     <Routes>
       <Route path='/' element={<Navigation/>}>
         <Route index={true} element={<Home/>}/>
-        <Route path='sign-in' element={<SignIn/>}/>
+        <Route path='shop' element={<Shop/>}/>
+        <Route path='auth' element={<Authentication/>}/>
 
       </Route>
     </Routes>
