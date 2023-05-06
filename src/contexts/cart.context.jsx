1e1093462c9b3fbfc53ff0f65788753f11616abc
Tahmid Sajin <tahmid.saj@gmail.com>
2023-05-06 17:13:41 -0400

    Adding updates to cart total

--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -51,12 +51,14 @@ export const CartContext = createContext({
     removeItemFromCart: () => {},
     clearItemFromCart: () => {},
     cartCount: 0,
+    cartTotal: 0,
 });
 
 export const CartProvider = ({ children }) => {
     const [isCartOpen, setIsCartOpen] = useState(false);
     const [cartItems, setCartItems] = useState([]);
     const [cartCount, setCartCount] = useState(0);
+    const [cartTotal, setCartTotal] = useState(0);
 
     useEffect(() => {
         const newCartCount = cartItems.reduce((total, cartItem) => {
@@ -64,7 +66,15 @@ export const CartProvider = ({ children }) => {
         }, 0);
 
         setCartCount(newCartCount);
-    }, [cartItems])
+    }, [cartItems]);
+
+    useEffect(() => {
+        const newCartTotal = cartItems.reduce((total, cartItem) => {
+            return total + cartItem.quantity * cartItem.price;
+        }, 0);
+
+        setCartTotal(newCartTotal);
+    }, [cartItems]);
 
     const addItemToCart = (productToAdd) => {
         setCartItems(addCartItem(cartItems, productToAdd));
@@ -84,9 +94,10 @@ export const CartProvider = ({ children }) => {
                 removeItemToCart, 
                 clearItemFromCart,
                 cartItems, 
-                cartCount };
+                cartCount,
+                cartTotal };
 
     return (
         <CartContext.Provider value={value}>{children}</CartContext.Provider>
-    )
-}
+    );
+};
--- src/routes/checkout/checkout.component.jsx
+++ src/routes/checkout/checkout.component.jsx
@@ -7,7 +7,7 @@ import CheckoutItem from '../../components/checkout-item/checkout-item.component
 import './checkout.styles.scss';
 
 const Checkout = () => {
-    const { cartItems } = useContext(CartContext);
+    const { cartItems, cartTotal } = useContext(CartContext);
 
     return (
         <div className="checkout-container">
@@ -41,7 +41,7 @@ const Checkout = () => {
                 })
                 }
 
-                <span className="total">Total: 0</span>
+                <span className="total">Total: ${cartTotal}</span>
         </div>
     );
 };
