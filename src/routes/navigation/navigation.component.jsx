e9d21cce5b726aa24328b5b4c1db28461c15de7a
Tahmid Sajin <tahmid.saj@gmail.com>
2023-05-06 03:22:59 -0400

    Adding cart dropdown and cart icon components

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -5,7 +5,11 @@ import { UserContext } from '../../contexts/user.context';
 import { signOutUser } from '../../utils/firebase/firebase.utils';
 
 import { ReactComponent as CrownLogo } from '../../assets/crown.svg';
+import CartIcon from '../../components/cart-icon/cart-icon.component';
+import CartDropdown from '../../components/cart-dropdown/cart-dropdown.component';
+
 import './navigation.styles.scss';
+
 
 const Navigation = () => {
   const { currentUser } = useContext(UserContext);
@@ -31,7 +35,10 @@ const Navigation = () => {
                       </Link>
                       )
                 }
+
+                <CartIcon/>
           </div>
+          <CartDropdown/>
         </div>
   
         <Outlet/>
