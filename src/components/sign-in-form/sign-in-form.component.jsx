4ef11b5a6a32bfee24467afdaf61ccd610718c08
Tahmid Sajin <tahmid.saj@gmail.com>
2023-05-05 23:53:45 -0400

    Setting up onAuthStateChange

--- src/components/sign-in-form/sign-in-form.component.jsx
+++ src/components/sign-in-form/sign-in-form.component.jsx
@@ -28,6 +28,7 @@ const SignInForm = () => {
 
     const signInWithGoogle = async () => {
         const { user } = await signInWithGooglePopup();
+        setCurrentUser(user);
         await createUserDocumentFromAuth(user);
     }
 
--- src/contexts/user.context.jsx
+++ src/contexts/user.context.jsx
@@ -1,4 +1,6 @@
-import { createContext, useState } from 'react';
+import { createContext, useState, useEffect } from 'react';
+
+import { onAuthStateChangedListener } from '../utils/firebase/firebase.utils';
 
 // as the actual value you want to access
 export const UserContext = createContext({
@@ -9,6 +11,16 @@ export const UserContext = createContext({
 export const UserProvider = ({ children }) => {
     const [currentUser, setCurrentUser] = useState(null);
     const value = { currentUser, setCurrentUser };
+
+    // signOutUser();
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChangedListener((user) => {
+            console.log(user);
+        });
+
+        return unsubscribe;
+    }, []);
 
     return <UserContext.Provider value={value}>{children}</UserContext.Provider>
 };
