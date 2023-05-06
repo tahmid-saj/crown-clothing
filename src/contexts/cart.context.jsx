3be8e49169f03e857dbdf1cd23b5066437aa81f1
Tahmid Sajin <tahmid.saj@gmail.com>
2023-05-06 17:04:11 -0400

    Adding cart quantity arrows

--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -37,12 +37,19 @@ const removeCartItem = (cartItems, cartItemToRemove) => {
     });
 };
 
+const clearCartItem = (cartItems, cartItemToClear) => {
+    return cartItems.filter((cartItem) => {
+        return cartItem.id !== cartItemToClear.id;
+    });
+};
+
 export const CartContext = createContext({
     isCartOpen: false,
     setIsCartOpen: () => {},
     cartItems: [],
     addItemToCart: () => {},
     removeItemFromCart: () => {},
+    clearItemFromCart: () => {},
     cartCount: 0,
 });
 
@@ -67,10 +74,15 @@ export const CartProvider = ({ children }) => {
         setCartItems(removeCartItem(cartItems, cartItemToRemove));
     };
 
+    const clearItemFromCart = (cartItemToClear) => {
+        setCartItems(clearCartItem(cartItems, cartItemToClear));
+    };
+
     const value = {isCartOpen, 
                 setIsCartOpen, 
                 addItemToCart,
                 removeItemToCart, 
+                clearItemFromCart,
                 cartItems, 
                 cartCount };
 
