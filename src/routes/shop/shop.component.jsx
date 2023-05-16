f2eef7f8d5c541460be13dfcb819c16292f51c91
Tahmid Sajin <tahmid.saj@gmail.com>
2023-05-15 22:25:21 -0400

    Adding single category page

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -7,6 +7,8 @@ import ProductCard from '../../components/product-card/product-card.component';
 import CategoryPreview from '../../components/category-preview/category-preview.component';
 import CategoriesPreview from '../categories-preview/categories-preview.component';
 
+import Category from "../category/category.component";
+
 import './shop.styles.scss';
 
 const Shop = () => {
@@ -14,6 +16,7 @@ const Shop = () => {
     return (
         <Routes>
             <Route index element={<CategoriesPreview/>}/>
+            <Route path=":category" element={<Category/>}/>
         </Routes>
     );
 };
