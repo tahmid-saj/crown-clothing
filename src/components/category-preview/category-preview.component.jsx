25037b486eacea504c554cd1db78d718bb9ef97b
Tahmid Sajin <tahmid.saj@gmail.com>
2023-05-15 21:57:52 -0400

    Adding category preview component

---
+++ src/components/category-preview/category-preview.component.jsx
@@ -0,0 +1,26 @@
+import "./category-preview.styles.scss";
+
+import ProductCard from '../product-card/product-card.component';
+
+const CategoryPreview = ({ title, products }) => {
+    return (
+        <div className="category-preview-container">
+            <h2>
+                <span className="title">{title.toUpperCase()}</span>
+            </h2>
+
+            <div className="preview">
+                {
+                    products.filter((_, idx) => {
+                        return idx < 4;
+                    })
+                    .map((product) => {
+                        return <ProductCard key={product.id} product={product}/>
+                    })
+                }
+            </div>
+        </div>
+    );
+};
+
+export default CategoryPreview;
--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -2,33 +2,23 @@ import { useContext, Fragment } from 'react';
 
 import { CategoriesContext } from '../../contexts/categories.context';
 import ProductCard from '../../components/product-card/product-card.component';
+import CategoryPreview from '../../components/category-preview/category-preview.component';
 import './shop.styles.scss';
 
 const Shop = () => {
     const { categoriesMap } = useContext(CategoriesContext);
 
     return (
-        <Fragment>
+        <div className="shop-container">
             {
                 Object.keys(categoriesMap).map(title => {
 
-                    return (
-                    <Fragment key={title}>
-                        <h2>{title}</h2>
-
-                        <div className="products-container">
-                        {categoriesMap[title].map((product) => {
-                            return (
-                                <ProductCard key={product.id} product={product}/>
-                            );
-                        })
-                        }
-                        </div>
-                    </Fragment>
-                    )
+                    const products = categoriesMap[title];
+                    
+                    return <CategoryPreview key={title} title={title} products={products}/>
                 })
             }
-        </Fragment>
+        </div>
     );
 };
 
