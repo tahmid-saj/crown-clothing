6e39286b4c70edf1e9a04ba9c1455b567f49b225
Tahmid Sajin <tahmid.saj@gmail.com>
2023-05-16 00:39:49 -0400

    Adding styled components for cart dropdown

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -6,7 +6,7 @@ import { CartContext } from '../../contexts/cart.context';
 import Button from '../button/button.component';
 import CartItem from '../cart-item/cart-item.component';
 
-import './cart-dropdown.styles.scss';
+import {CartDropdownContainer, EmptyMessage, CartItems} from './cart-dropdown.styles.jsx';
 
 const CartDropdown = () => {
     const { cartItems } = useContext(CartContext);
@@ -17,15 +17,19 @@ const CartDropdown = () => {
     }
 
     return (
-        <div className="cart-dropdown-container">
-            <div className='cart-items'>
-                {cartItems.map((item) => {
-                    return <CartItem key={item.id} cartItem={item}/>
-                })}
-            </div>
+        <CartDropdownContainer className="cart-dropdown-container">
+            <CartItems className='cart-items'>
+                {
+                    cartItems.length ? (cartItems.map((item) => {
+                        return <CartItem key={item.id} cartItem={item}/>
+                    })) : (
+                        <EmptyMessage>Your cart is empty</EmptyMessage>
+                    )
+                }
+            </CartItems>
 
             <Button onClick={goToCheckoutHandler}>GO TO CHECKOUT</Button>
-        </div>
+        </CartDropdownContainer>
     );
 };
 
