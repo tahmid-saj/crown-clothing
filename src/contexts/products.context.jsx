edd5f6db6add20d36e609caae02c565a24cefb41
Tahmid Sajin <tahmid.saj@gmail.com>
2023-05-15 20:47:57 -0400

    Adding shop-data categories to firestore db

--- src/contexts/products.context.jsx
+++ src/contexts/products.context.jsx
@@ -1,4 +1,6 @@
-import { createContext, useState } from 'react';
+import { createContext, useState, useEffect } from 'react';
+
+import { addCollectionAndDocuments } from '../utils/firebase/firebase.utils.js';
 
 import SHOP_DATA from '../shop-data.js';
 
@@ -10,6 +12,10 @@ export const ProductsContext = createContext({
 export const ProductsProvider = ({ children }) => {
     const [products, setProducts] = useState([]);
     const value = { products };
+
+    useEffect(() => {
+        addCollectionAndDocuments("categories", SHOP_DATA)
+    }, []);
 
     return (
         <ProductsContext.Provider value={value}>{children}</ProductsContext.Provider>
