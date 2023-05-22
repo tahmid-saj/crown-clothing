279d9dee79a79bd485ebe45c5d53887f12d69044
Tahmid Sajin <tahmid.saj@gmail.com>
2023-05-22 12:42:55 -0400

    Adding reducer for user

--- src/contexts/user.context.jsx
+++ src/contexts/user.context.jsx
@@ -9,8 +9,35 @@ export const UserContext = createContext({
     setCurrentUser: () => null,
 });
 
+export const USER_ACTION_TYPES = {
+    SET_CURRENT_USER: "SET_CURRENT_USER",
+};
+
+const useReducer = (state, action) => {
+    const { type, payload } = action;
+
+    switch (type) {
+        case USER_ACTION_TYPES.SET_CURRENT_USER:
+            return {
+                ...state,
+                currentUser: payload
+            };
+        default:
+            throw new Error(`Unhandled type ${type} in the userReducer`);
+    }
+}
+
+const INITIAL_STATE = {
+    currentUser: null
+};
+
 export const UserProvider = ({ children }) => {
-    const [currentUser, setCurrentUser] = useState(null);
+    const [ state, dispatch ] = useReducer(useReducer, INITIAL_STATE);
+    const { currentUser } = state;
+    const setCurrentUser = (user) => {
+        dispatch({ type: USER_ACTION_TYPES.SET_CURRENT_USER, payload: user });
+    };
+
     const value = { currentUser, setCurrentUser };
 
     // signOutUser();
