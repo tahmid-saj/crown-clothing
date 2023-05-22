07d380da791ab85c840da7794a7f5284ea1d8c43
Tahmid Sajin <tahmid.saj@gmail.com>
2023-05-22 17:17:07 -0400

    Converting categories to redux

--- src/store/root.reducer.js
+++ src/store/root.reducer.js
@@ -1,7 +1,10 @@
 import { combineReducers } from 'redux';
 
 import { userReducer } from "./user/user.reducer";
+import { categoriesReducer } from './categories/category.reducer';
 
 export const rootReducer = combineReducers({ 
-    user: userReducer
+    user: userReducer,
+    categories: categoriesReducer,
 });
+
