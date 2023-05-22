c879e2b37899fa9abe66fcf7f15979f741120129
Tahmid Sajin <tahmid.saj@gmail.com>
2023-05-22 16:31:01 -0400

    Creating reducer.utils

--- src/App.js
+++ src/App.js
@@ -1,11 +1,38 @@
 import { Routes, Route, Outlet } from 'react-router-dom';
+
+import { useDispatch } from 'react-redux';
+
 import Home from './routes/home/home.component';
 import Navigation from './routes/navigation/navigation.component';
 import Authentication from './routes/authentication/authentication.component';
 import Shop from './routes/shop/shop.component';
 import Checkout from './routes/checkout/checkout.component';
 
+import { createContext, useState, useEffect, useReducer } from 'react';
+
+import { createAction } from './utils/reducer/reducer.utils';
+
+import { onAuthStateChangedListener, 
+        createUserDocumentFromAuth } from '../utils/firebase/firebase.utils';
+
+import { setCurrentUser } from './store/user/user.action';
+
 const App = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+      const unsubscribe = onAuthStateChangedListener((user) => {
+          
+          if (user) {
+              createUserDocumentFromAuth(user);
+          }
+
+          setCurrentUser(user);
+      });
+
+      return unsubscribe;
+  }, [dispatch]);
+
   return (
     <Routes>
       <Route path='/' element={<Navigation/>}>
