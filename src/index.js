9455c7bc34c75ebeaa2697efe8606d82718dac5d
Tahmid Sajin <tahmid.saj@gmail.com>
2023-05-28 12:46:32 -0400

    Adding redux persist

--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 
+import { PersistGate } from 'redux-persist/integration/react';
+
 import './index.scss';
 import App from './App';
 import { UserProvider } from './contexts/user.context';
 import { CartProvider } from './contexts/cart.context';
-import { store } from "./store/store";
+import { store, persistor } from "./store/store";
 
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
@@ -15,9 +17,11 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
   <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
       <BrowserRouter>
             <App />
       </BrowserRouter>
+      </PersistGate>
   </Provider>
   </React.StrictMode>
 );
--- src/store/store.js
+++ src/store/store.js
@@ -1,6 +1,9 @@
 import { compose, createStore, applyMiddleware } from 'redux';
 import { logger } from 'redux-logger';
 
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage"
+
 import { rootReducer } from './root.reducer';
 
 const loggerMiddleware = (store) => (next) => (action) => {
@@ -17,6 +20,14 @@ const loggerMiddleware = (store) => (next) => (action) => {
     console.log("Next state: ", store.getState());
 }
 
+const persistConfig = {
+    key: "root",
+    storage: storage,
+    blacklist: ["user"],
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 const middleWares = [loggerMiddleware];
 
 const composedEnhancers = compose(applyMiddleware(...middleWares));
@@ -24,4 +35,6 @@ const composedEnhancers = compose(applyMiddleware(...middleWares));
 export const store = createStore(rootReducer, undefined, composedEnhancers);
 
 // Root-reducer
+
+export const persistor = persistStore(store);
 
