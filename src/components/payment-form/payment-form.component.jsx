943c0b7cdfcbd820de9c133f9ebbc377a5c899b2
Tahmid Sajin <tahmid.saj@gmail.com>
2023-06-20 03:54:30 -0400

    Finalizing stripe payment

--- src/components/payment-form/payment-form.component.jsx
+++ src/components/payment-form/payment-form.component.jsx
@@ -15,12 +15,39 @@ const PaymentForm = () => {
             return;
         }
 
+        const response = await fetch("/.netlify/functions/create-payment-intent", {
+            method: 'post',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ amount: 10000 })
+        }).then(res => res.json());
+
+        const {paymentIntent: { client_secret }} = response;
         
+        console.log(client_secret);
+
+        const paymentResult = await stripe.confirmCardPayment(client_secret, {
+            payment_method: {
+                card: elements.getElement(CardElement),
+                billing_details: {
+                    name: "YZ"
+                }
+            }
+        });
+
+        if (paymentResult.error) {
+            alert(paymentResult.error);
+        } else {
+            if (paymentResult.paymentIntent.status === "succeeded") {
+                alert("Payment successfull");
+            }
+        }
     };
 
     return (
         <PaymentFormContainer>
-            <FormContainer>
+            <FormContainer onSubmit={paymentHandler}>
                 <h2>Credit card payment: </h2>
 
                 <CardElement/>
