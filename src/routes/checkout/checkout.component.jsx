8210968e5cfd42587930a55fb5e3276b73a0061e
Tahmid Sajin <tahmid.saj@gmail.com>
2023-06-20 03:14:32 -0400

    Adding payment form

--- src/routes/checkout/checkout.component.jsx
+++ src/routes/checkout/checkout.component.jsx
@@ -3,6 +3,7 @@ import { useContext } from 'react';
 import { CartContext } from '../../contexts/cart.context';
 
 import CheckoutItem from '../../components/checkout-item/checkout-item.component';
+import PaymentForm from '../../components/payment-form/payment-form.component';
 
 import './checkout.styles.scss';
 
@@ -42,6 +43,7 @@ const Checkout = () => {
                 }
 
                 <span className="total">Total: ${cartTotal}</span>
+                <PaymentForm/>
         </div>
     );
 };
