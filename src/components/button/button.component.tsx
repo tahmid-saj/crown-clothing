f3999cfb48920bacc680233e11746df29fadae05
Tahmid Sajin <tahmid.saj@gmail.com>
2023-06-20 23:33:04 -0400

    Typing button component

--- src/components/button/button.component.tsx
+++ src/components/button/button.component.tsx
@@ -1,21 +1,28 @@
 import { BaseButton, GoogleSignInButton, InvertedButton, ButtonSpinner } from './button.styles.jsx';
 
-export const BUTTON_TYPE_CLASSES = {
-    base: 'base',
-    google: 'google-sign-in',
-    inverted: 'inverted'
+import { FC, ButtonHTMLAttributes } from "react";
+
+export enum BUTTON_TYPE_CLASSES {
+    base = 'base',
+    google = 'google-sign-in',
+    inverted = 'inverted'
 };
 
-const getButton = (buttonType = BUTTON_TYPE_CLASSES.base) => {
+const getButton = (buttonType = BUTTON_TYPE_CLASSES.base): typeof BaseButton => {
     return ({
         [BUTTON_TYPE_CLASSES.base]: BaseButton,
         [BUTTON_TYPE_CLASSES.google]: GoogleSignInButton,
         [BUTTON_TYPE_CLASSES.inverted]: InvertedButton,
 
     }[buttonType]);
-}
+};
 
-const Button = ({ children, buttonType, isLoading, ...otherProps }) => {
+export type ButtonProps = {
+    buttonType?: BUTTON_TYPE_CLASSES;
+    isLoading?: boolean;
+} & ButtonHTMLAttributes<HTMLButtonElement>;
+
+const Button: FC<ButtonProps> = ({ children, buttonType, isLoading, ...otherProps }: ButtonProps) => {
     const CustomButton = getButton(buttonType);
 
     return (
